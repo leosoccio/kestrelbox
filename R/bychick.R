24a7291d8ee4e3c9c70efc541f6c723dbf25f736
leosoccio <leosoccio850@gmail.com>
2025-11-04 00:26:51 -0500

    hopefully done?

--- R/bychick.R
+++ R/bychick.R
@@ -1,16 +1,17 @@
-#' Kestrel nesting data in which each observation is a single chick.
+#' Kestrel nesting data organized by chick
 #'
+#' A dataset containing nesting data for kestrel chicks in Pennsylvania and New Jersey. Each row represents one chick.
 #' 
-#' @format A tibble with 2027 rows and 8 variables:
+#' @format A data frame with 2027 rows and 8 variables:
 #' \describe{
 #'   \item{box_id}{ID number for the box the chick was nesting in for compatability with the bybox dataset}
 #'   \item{year}{The year in which the chick was observed} 
 #'   \item{age}{Age of the chick when banded, in days}
 #'   \item{weight}{Weight of the chick when banded, in grams}
 #'   \item{band_date}{Date on which the chick was banded}
-#'   \item{fledge_date}{Date on which the chick turned 30 days old, assumed to be when it fledged}
+#'   \item{fledge_date}{Date on which the chick turned 30 days old}
 #'   \item{sex}{Sex of the chick. M for male, F for female, U for unknown sex}
 #'   \item{study_area}{Indicates whether the chick was in central PA, New Jersey, or Lancaster, PA}
 #' }
-#' @source \item{Kestrel data collected by Steve Eisenhauer.}
+#' @source Kestrel data collected by Steve Eisenhauer.
 "bychick"
